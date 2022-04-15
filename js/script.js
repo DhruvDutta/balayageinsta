2bf4342a4a0b9979c47ef06f779386fb9e9640f4
DhruvDutta <dhrvdutta@gmail.com>
2022-04-15 14:05:36 +0530

    date

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ function book(){
     var location = document.getElementById('location').value;
     var process = document.getElementById('process').value;
     var contact = document.getElementById('from').value;
-    var msg = `Name:${name}\nLocation:${location}\nProcess:${process}\nContact:${contact}`
+    var date = document.getElementById('date').value;
+    var msg = `Name:${name}\nAddress:${location}\nService:${process}\nContact:${contact}\nDate:${date}`
     msg= encodeURIComponent(msg)
     window.open(`mailto:balayageinsta@gmail.com?subject=Appointment&body=${msg}`);
 }
